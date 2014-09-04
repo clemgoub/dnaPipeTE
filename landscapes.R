e2e2817bb391aae69f4949f1c853d74e8824eb10
Clement Goubert <clement.goubert@univ-lyon1.fr>
2014-09-04 15:22:42 +0200

    3 new files for landscapes graphs (landscapes.sh, landscapes.R and colors)

---
+++ landscapes.R
@@ -0,0 +1,18 @@
+#!/bin/Rscript
+
+library(ggplot2)
+
+#read the table with reads and identity selected for landscape analysis
+read.table("reads_landscape")->land
+names(land)=c("id", "annot", "fam1", "fam")
+land$div=100-land$id
+
+#read the corresponding factor order and color table
+read.table("factors_and_colors")->fac_col
+
+#order factors and colors
+land$fam1=factor(land$fam1, levels=as.character(fac_col$V1))
+
+#plot the landscape graph
+ggplot(land, aes(div, fill=fam1))+geom_histogram(binwidth=1.1)+labs(list(x="div", y="reads count"))+scale_x_continuous(limits = c(0, 35))+scale_fill_manual(values=as.character(fac_col$V3))
+ggsave("landscape.pdf")
