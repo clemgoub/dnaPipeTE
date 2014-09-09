c27db7217d33081730c8686079431304e0853650
Clement Goubert <clement.goubert@univ-lyon1.fr>
2014-09-09 14:57:54 +0200

    changement des couleurs pieChart.R

--- pieChart.R
+++ pieChart.R
@@ -11,19 +11,19 @@ names(counts)=c("class","reads")
 Single_or_low_copy=counts$reads[9]-sum(counts$reads[1:8])
 perc=signif(c(counts$reads[1:8]/counts$reads[9],Single_or_low_copy/counts$reads[9])*100,3)
 #pie(c(counts$reads[1:8],Single_or_low_copy),paste(c(as.character(counts$class[-9]),"Single or low copy DNA"), perc, "%"), col=rainbow(9))
-pal=c("firebrick1","darkorange1","forestgreen","dodgerblue3","darkmagenta","grey","chartreuse","gold","brown")
+pal=c("#33cc33ff","#6699ffff","#B966F4","salmon","dimgrey","chartreuse","gold","black","#d3d3d3ff")
 #pie(c(counts$reads[1:8],Single_or_low_copy),paste(c(as.character(counts$class[-9]),"Single or low copy DNA"), perc, "%"), col=pal)
 
 png(file=paste(folder,"TEs_piechart.png",sep="/"), width=1000, height=600)
 
-pie(c(counts$reads[1:8],Single_or_low_copy),paste(perc, "%"), col=pal, clockwise=T)
+pie(c(counts$reads[1:8],Single_or_low_copy),paste(perc, "%"), col=pal, clockwise=T, border="white")
 legend(1,0.5,paste(c(as.character(counts$class[-9]),"Single or low copy DNA")),fill=pal)
 
 dev.off()
 
 pdf(file=paste(folder,"TEs_piechart.pdf",sep="/"), width=16, height=10)
 
-pie(c(counts$reads[1:8],Single_or_low_copy),paste(perc, "%"), clockwise=T, col=pal)
+pie(c(counts$reads[1:8],Single_or_low_copy),paste(perc, "%"), clockwise=T, col=pal, border="white")
 legend(1,0.5,paste(c(as.character(counts$class[-9]),"Single or low copy DNA")),fill=pal)
 
 dev.off()
