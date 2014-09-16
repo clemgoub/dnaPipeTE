dd8b64a7627eccdd1cbe9dfa6bf0179a04734686
Clement Goubert <clement.goubert@univ-lyon1.fr>
2014-09-16 15:06:04 +0200

    cool

---
+++ landscapes2.R
@@ -0,0 +1,26 @@
+#!/usr/bin/Rscript
+
+library(ggplot2)
+
+#read the table with reads and identity selected for landscape analysis
+Args = commandArgs()
+
+file1 = Args[6]
+file2 = Args[7]
+
+land = read.table(file1)
+
+
+#read.table("reads_landscape")->land
+names(land)=c("id", "annot", "fam1", "fam")
+land$div=100-land$id
+
+#read the corresponding factor order and color table
+fac_col = read.table(file2)
+
+#order factors and colors
+land$fam1=factor(land$fam, levels=as.character(fac_col$V1))
+
+#plot the landscape graph
+ggplot(land, aes(div, fill=fam))+geom_histogram(binwidth=1.1)+labs(list(x="div", y="reads count"))+scale_x_continuous(limits = c(0, 35))+scale_fill_manual(values=as.character(fac_col$V3))
+ggsave("landscape.pdf")
