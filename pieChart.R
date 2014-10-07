90e049a099af7bcc4550de2b898ad339fc779b81
Clement Goubert <clement.goubert@univ-lyon1.fr>
2014-10-07 13:03:32 +0200

    Version OK sauf nb exact de sampling: utiliser "dnaPipeTE.py"

--- pieChart.R
+++ pieChart.R
@@ -19,7 +19,7 @@ subpalette = subset(palette, palette$class %in% subcounts$class)
 
 
 Single_or_low_copy=counts$reads[length(counts$reads)]-sum(counts$reads[1:(length(counts$reads)-1)])
-perc=signif(    c(      ((counts$reads[1:(length(counts$reads)-1)])/(counts$reads[length(counts$reads)]) )    , Single_or_low_copy/(counts$reads[length(counts$reads)]*100),    3)      )
+perc=round(    c(      ((subcounts$reads[1:(length(subcounts$reads)-1)])/(counts$reads[length(counts$reads)]) )    , (Single_or_low_copy/(counts$reads[length(counts$reads)]))),   4      )
 
 
 #pie(c(counts$reads[1:(length(counts$reads)-1),Single_or_low_copy),paste(c(as.character(counts$class[-length(counts$reads)]),"Single or low copy DNA"), perc, "%"), col=rainbow(9))
@@ -28,15 +28,16 @@ perc=signif(    c(      ((counts$reads[1:(length(counts$reads)-1)])/(counts$read
 
 png(file=paste(folder,"TEs_piechart.png",sep="/"), width=1000, height=600)
 
-pie(c(subcounts$reads[1:(length(subcounts$reads)-1)], Single_or_low_copy), paste(perc, "%"), col=c(as.character(subpalette$col),"#d3d3d3ff"), clockwise=T, border="white")
+pie(c(subcounts$reads[1:(length(subcounts$reads)-1)], Single_or_low_copy), paste(perc*100, "%"), col=c(as.character(subpalette$col),"#d3d3d3ff"), clockwise=T, border="white")
 
 legend(1,0.5,paste(c(as.character(subcounts$class[-length(subcounts$reads)]),"Single or low copy DNA")),fill=c(as.character(subpalette$col),"#d3d3d3ff"))
 
 dev.off()
 
-#pdf(file=paste(folder,"TEs_piechart.pdf",sep="/"), width=16, height=10)
+pdf(file=paste(folder,"TEs_piechart.pdf",sep="/"), width=16, height=10)
 
-#pie(c(counts$reads[1:(length(counts$reads)-1),Single_or_low_copy),paste(perc, "%"), clockwise=T, col=pal, border="white")
-#legend(1,0.5,paste(c(as.character(counts$class[-length(counts$reads)]),"Single or low copy DNA")),fill=pal)
+pie(c(subcounts$reads[1:(length(subcounts$reads)-1)], Single_or_low_copy), paste(perc*100, "%"), col=c(as.character(subpalette$col),"#d3d3d3ff"), clockwise=T, border="white")
 
-#dev.off()
+legend(1,0.5,paste(c(as.character(subcounts$class[-length(subcounts$reads)]),"Single or low copy DNA")),fill=c(as.character(subpalette$col),"#d3d3d3ff"))
+
+dev.off()
