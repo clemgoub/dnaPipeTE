c0aee2630a5414a907a29b6a81b2aea69fbe4b1f
Clement Goubert <goubert.clement@gmail.com>
2016-04-13 14:36:00 +0200

    remove reads_per_comp graphs and allow base_per_comp if sample_size provided

--- graph.R
+++ graph.R
@@ -34,7 +34,7 @@ as.data.frame(idpc)->idpc
 cbind(rpc, idpc$idpc, alnpc$aln_size)->r_id_pc
 names(r_id_pc)=c("component","reads","mean_id", "aln_size")
 
-r_id_pc[order(r_id_pc$reads, decreasing=T),]->r_id_pc
+r_id_pc[order(r_id_pc$aln_size, decreasing=T),]->r_id_pc
 
 write.table(r_id_pc, file=paste(folder,"reads_per_component_sorted.txt",sep="/"), quote=F)
 
@@ -44,29 +44,29 @@ seuil=sum(subset(r_id_pc, r_id_pc$reads>=0.0001*total[1,1])$reads)/total[1,1]
 
 #graphs 
 
-pdf(file=paste(folder,"Reads_to_components.pdf",sep="/"))
-barplot(r_id_pc$reads, r_id_pc$reads/total[1,1], xlim=c(0,1), ylim=c(0,max(r_id_pc$reads)+1000),xaxt='n', ylab="reads per component", xlab="genome proportion")
-segments(0,0,1,0)
-axis(1,c(0,0.25,0.5,0.75,1))
-rect(c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(0,0),c(1,1),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#00FF0010")
-rect(c(0,0),c(1,1),c(seuil,seuil),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000010")
-rect(c(seuil,seuil),c(1,1),c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000005")
-axis(1,signif(sum(r_id_pc$reads/total[1,1]),3))
-legend(0.75,max(r_id_pc$reads)*2/3, legend=c("repeats","rep < 0.01 %","single"),fill=c("#FF000024","#FF000014","#00FF0020"), bg="white")
-dev.off()
+# pdf(file=paste(folder,"Reads_to_components.pdf",sep="/"))
+# barplot(r_id_pc$reads, r_id_pc$reads/total[1,1], xlim=c(0,1), ylim=c(0,max(r_id_pc$reads)+1000),xaxt='n', ylab="reads per component", xlab="genome proportion")
+# segments(0,0,1,0)
+# axis(1,c(0,0.25,0.5,0.75,1))
+# rect(c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(0,0),c(1,1),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#00FF0010")
+# rect(c(0,0),c(1,1),c(seuil,seuil),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000010")
+# rect(c(seuil,seuil),c(1,1),c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000005")
+# axis(1,signif(sum(r_id_pc$reads/total[1,1]),3))
+# legend(0.75,max(r_id_pc$reads)*2/3, legend=c("repeats","rep < 0.01 %","single"),fill=c("#FF000024","#FF000014","#00FF0020"), bg="white")
+# dev.off()
 
-png(file=paste(folder,"Reads_to_components.png",sep="/"), width=800, height=600)
-barplot(r_id_pc$reads, r_id_pc$reads/total[1,1], xlim=c(0,1), ylim=c(0,max(r_id_pc$reads)+1000),xaxt='n', ylab="reads per component", xlab="genome proportion")
-segments(0,0,1,0)
-axis(1,c(0,0.25,0.5,0.75,1))
-rect(c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(0,0),c(1,1),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#00FF0010")
-rect(c(0,0),c(1,1),c(seuil,seuil),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000010")
-rect(c(seuil,seuil),c(1,1),c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000005")
-axis(1,signif(sum(r_id_pc$reads/total[1,1]),3))
-legend(0.75,max(r_id_pc$reads)*2/3, legend=c("repeats","rep < 0.01 %","single"),fill=c("#FF000024","#FF000014","#00FF0020"), bg="white")
-dev.off()
+# png(file=paste(folder,"Reads_to_components.png",sep="/"), width=800, height=600)
+# barplot(r_id_pc$reads, r_id_pc$reads/total[1,1], xlim=c(0,1), ylim=c(0,max(r_id_pc$reads)+1000),xaxt='n', ylab="reads per component", xlab="genome proportion")
+# segments(0,0,1,0)
+# axis(1,c(0,0.25,0.5,0.75,1))
+# rect(c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(0,0),c(1,1),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#00FF0010")
+# rect(c(0,0),c(1,1),c(seuil,seuil),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000010")
+# rect(c(seuil,seuil),c(1,1),c((sum(r_id_pc$reads/total[1,1])),(sum(r_id_pc$reads/total[1,1]))),c(max(r_id_pc$reads)+1000,max(r_id_pc$reads)+1000), col="#FF000005")
+# axis(1,signif(sum(r_id_pc$reads/total[1,1]),3))
+# legend(0.75,max(r_id_pc$reads)*2/3, legend=c("repeats","rep < 0.01 %","single"),fill=c("#FF000024","#FF000014","#00FF0020"), bg="white")
+# dev.off()
 
-if(GB != 0){
+# if(GB != 0){
 
 #bases par component
 pdf(file=paste(folder,"Bases_per_components.pdf",sep="/"))
@@ -91,4 +91,4 @@ axis(1,signif(sum(as.vector(r_id_pc$aln_size)/GB),3))
 legend(0.75,max(as.vector(r_id_pc$aln_size))*2/3, legend=c("repeats","rep < 0.01 %","single"),fill=c("#FF000024","#FF000014","#00FF0020"), bg="white")
 dev.off()
 
-}
+# }
